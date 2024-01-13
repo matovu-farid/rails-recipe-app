c7b64e6939a67cd2c9140df1d7e8fdc662b5fc5f
Farid Matovu <matovu90@gmail.com>
2024-01-13 10:38:18 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :recipe_foods, only: [:update,:create, :destroy] 
   resources :foods , except: [:show]
   devise_for :users
+  
   get '/public_recipes', to: 'recipes#public'
   get '/general_shopping_list/:recipe_id', to: 'shopping_list#index', as: 'shopping_list'
   root to: "recipes#public"
